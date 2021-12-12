15b637918bf33c88e63cd1525dc9bd465f633623
jariryyousef@gmail.com <jariryyousef@gmail.com>
2021-12-12 15:12:31 +0200

    new PR for favorite_reviews_table

--- Products/models.py
+++ Products/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
 
 
 
+
 class Favorite(models.Model):
     user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
     product_favorite = models.ForeignKey(Product, on_delete=models.CASCADE)
