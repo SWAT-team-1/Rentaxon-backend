8da7f8db98ff0779ebedc150f791c3cc68337b61
Haneen <haneen.hashlamoun@gmail.com>
2021-12-12 22:45:44 +0200

    heroku fix

--- Rentaxon/settings.py
+++ Rentaxon/settings.py
@@ -56,7 +56,7 @@ CSRF_TRUSTED_ORIGINS = ['https://rentaxon-api.herokuapp.com']
 CORS_REPLACE_HTTPS_REFERER = True
 
 CSRF_COOKIE_DOMAIN = 'herokuapp.com'
-SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = False
 INSTALLED_APPS = [
     "whitenoise.runserver_nostatic",
     'django.contrib.admin',
